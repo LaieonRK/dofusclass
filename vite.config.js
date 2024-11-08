a2d4fe8e222341d3ea1f0185836067a1284babe7
Ryan <45099838+LaieonRK@users.noreply.github.com>
2024-11-08 15:12:08 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: "/dofusclass/"
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+  base: './'
 }));
