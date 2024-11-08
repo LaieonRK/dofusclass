b1287241776b2062ae66642dcac2e47449cde08b
Ryan <45099838+LaieonRK@users.noreply.github.com>
2024-11-08 13:27:57 +0100

    bug local

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: mode === 'production' ? '/dofusclass/' : '/', // Utilise /dofusclass uniquement en production
-}))
+  base: mode === 'production' ? '/dofusclass/' : '/', // Utilise '/dofusclass/' uniquement en production
+}));
